f9813573412907ab212d47a99f0b399f95c06adc
Joseph Dotson <jdotson@ibs.com>
2015-05-06 11:33:36 -0400

    added optional configurations

--- server/config.js
+++ server/config.js
@@ -1,1 +1,9 @@
-process.env.MAIL_URL = "smtp:dotsonjb14:Garden817@smtp.sendgrid.net:587";
+//Accounts.urls.enrollAccount = function (token) {
+//    return "#/enroll-account/" + token;
+//};
+
+//Accounts.urls.resetPassword = function (token) {
+//    return "#/reset-password/" + token;
+//};
+
+//process.env.MAIL_URL = "smtp:dotsonjb14:Garden817@smtp.sendgrid.net:587";
