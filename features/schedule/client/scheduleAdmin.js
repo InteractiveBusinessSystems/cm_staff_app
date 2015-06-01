389e881df220388dec25ae3fae8981022000772e
Scott Zischerk <szischerk@ibs.com>
2015-06-01 10:33:35 -0400

    Schedule Admin changes

--- features/schedule/client/scheduleAdmin.js
+++ features/schedule/client/scheduleAdmin.js
@@ -1,13 +1,49 @@
-/**
- * Created by szischerk on 5/12/2015.
- */
-
-Meteor.call("getSchedule", function(err, data) {
-    Session.set('sessionList', data);
-});
+SessionList = new Meteor.Collection('session');
+Meteor.subscribe('sessionList');
 
 Template.scheduleAdmin.helpers ({
     "sessionList": function(){
-        return Session.get('sessionList');
+        if (SessionList.find().count() <= 0) return false;
+        var sessionsForDate = SessionList.find({$where: function(){ return moment(this.SessionStartTime).format('M-D-YY') == Session.get('selectedScheduleDate')}}).fetch();
+        var sessionsGrouped = _.toArray(_.groupBy(sessionsForDate, function(item){ return item.Rooms; }));
+        //return _.first(sessionsGrouped, 1);
+        return sessionsGrouped;
+    },
+    "dateList": function(){
+        var data = SessionList.find().fetch();
+        var distinctData = _.uniq(data, false, function(d) {return new Date(d.SessionStartTime).toDateString()});
+        var dates = _.map(distinctData, function(item){ return moment(item.SessionStartTime).format('M-D-YY');});
+        if(dates.length > 0)
+            Session.set('selectedScheduleDate', dates[0]);
+        return dates;
     }
+});
+
+Template.scheduleAdmin.events({
+    'click #scheduleRefresh': function(){
+        Meteor.call('refreshSchedule', function(err, data) {
+            alert('Refreshed Schedule');
+        });
+    },
+
+    'click .datePicker': function(){
+        Session.set('selectedScheduleDate', this.toString());
+    },
+
+    'click .scheduleAdminSession': function(){
+        alert(this.Title);
+    }
+});
+
+Template.registerHelper('getTop', function(startTime){
+    var x = moment.duration(startTime) / 60000;
+    x = x - 445;
+    return x.toString() + 'px';
+});
+
+Template.registerHelper('getHeight', function(startTime, endTime){
+    var x = moment.duration(startTime) / 60000;
+    var y = moment.duration(endTime) / 60000;
+    var z = (y-x);
+    return z.toString() + 'px';
 });
--- features/schedule/server/scheduleadmin.js
+++ features/schedule/server/scheduleadmin.js
@@ -1,16 +1,34 @@
-/**
- * Created by szischerk on 5/12/2015.
- */
-Meteor.methods({getSchedule: function () {
-    this.unblock();
-    try {
-        var result = HTTP.get("https://cmprod-speakers.azurewebsites.net/api/sessionsdata");
-        return result.data.filter(function(item){
-            return item.SessionType === "Pre-Compiler" ||
-                item.SessionType === "Regular Session";
-        });
-    } catch (e) {
-        // Got a network error, time-out or HTTP error in the 400 or 500 range.
-        return '';
+SessionList = new Meteor.Collection('session');
+Meteor.publish('sessionList', function(){
+    return SessionList.find();
+});
+
+Meteor.methods({
+    refreshSchedule: function(){
+        this.unblock();
+        try {
+            var result = HTTP.get("https://cmprod-speakers.azurewebsites.net/api/sessionsdata");
+            //var result = HTTP.get("http://localhost:3000/public/data/sessionsdata.xml");
+            var sessions =  result.data.filter(function(item){
+                return item.SessionType === "Pre-Compiler" ||
+                    item.SessionType === "Regular Session";
+            });
+
+            _.map(sessions, function(session){
+                if(SessionList.findOne({'Id':session.Id })) {
+                    SessionList.update({'Id':session.Id }, session);
+                }
+                else{
+                    SessionList.insert(session);
+                }
+            });
+
+
+        } catch (e) {
+            // Got a network error, time-out or HTTP error in the 400 or 500 range.
+            return '';
+        }
     }
-}});
+    }
+);
+
