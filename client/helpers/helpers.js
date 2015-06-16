e4bb08d293bfcb678dbadaf0c599a3e57a32e991
Scott Zischerk <szischerk@ibs.com>
2015-06-16 15:54:39 -0400

    Removed redundant helpers.js file

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -1,3 +1,15 @@
 Template.registerHelper('formatToTime', function (dateTime) {
     return moment(dateTime).format('h:mma');
 });
+
+Template.registerHelper("arrayJoin", function (arr) {
+    if (arr == null)
+        return "No Group";
+    else
+        return arr.join(",");
+});
+
+Template.registerHelper("isAdmin", function (user) {
+    var _user = user || Meteor.user();
+    return _user.role == "admin";
+});
