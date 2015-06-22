6897ec829957d79a6216227730efd40c3b96a701
Scott Zischerk <szischerk@ibs.com>
2015-06-22 08:36:41 -0400

    .less files changed

--- features/checkin/client/checkinsession.js
+++ features/checkin/client/checkinsession.js
@@ -6,7 +6,7 @@ Template.checkinsession.helpers({
         return SessionList.find({'_id': Router.current().params.sessionId});
     },
     'sessionStatus': function () {
-        if (this.checkInInfo.proctorCheckInTime === ""){
+        if (this.checkInInfo.proctorCheckInTime === "") {
             return "Not Started";
         }
         else if (this.checkInInfo.proctorCheckInTime !== "" && this.checkInInfo.sessionStartTime === "") {
@@ -19,97 +19,97 @@ Template.checkinsession.helpers({
             return "Done";
         }
     },
-    'checkInStatusColor': function(){
-        if (this.checkInInfo.proctorCheckInTime !== ""){
+    'checkInStatusColor': function () {
+        if (this.checkInInfo.proctorCheckInTime !== "") {
             return "#666666";
         }
         return '#f67e00';
     },
-    'startTimeStatusColor': function(){
-        if (this.checkInInfo.sessionStartTime !== ""){
+    'startTimeStatusColor': function () {
+        if (this.checkInInfo.sessionStartTime !== "") {
             return "#666666";
         }
         return '#f67e00';
     },
-    'endTimeStatusColor': function(){
-        if (this.checkInInfo.sessionEndTime !== ""){
+    'endTimeStatusColor': function () {
+        if (this.checkInInfo.sessionEndTime !== "") {
             return "#666666";
         }
         return '#f67e00';
     },
-    'modalData': function(){
+    'modalData': function () {
         return SessionList.findOne({'_id': Router.current().params.sessionId});
     },
-    'modalTitle': function(){
+    'modalTitle': function () {
         _modalDep.depend();
         if (_editAttendanceType === 10) return '10 Minute';
         return '50 Minute';
     },
-    'numberSelected': function(num){
+    'numberSelected': function (num) {
         _modalDep.depend();
         var attNum = _editAttendanceType === 10 ? this.checkInInfo.attendees10 : this.checkInInfo.attendees50;
         var numArray = num.toString().split('');
         var attNumArray = attNum.toString().padZero(3).split('');
 
-        if(numArray[0] == attNumArray[3-numArray.length]) return 'selected';
+        if (numArray[0] == attNumArray[3 - numArray.length]) return 'selected';
         return '';
     }
 });
 
 
 Template.checkinsession.events({
-    'click #startTime': function(e){
-        if(this.checkInInfo.sessionStartTime === '') {
+    'click #startTime': function (e) {
+        if (this.checkInInfo.sessionStartTime === '') {
             this.checkInInfo.sessionStartTime = new Date();
         } else {
             this.checkInInfo.sessionStartTime = '';
         }
         Meteor.call('saveCheckInInfo', this._id, this.checkInInfo);
     },
-    'click #endTime': function(e){
-        if(this.checkInInfo.sessionEndTime === '') {
+    'click #endTime': function (e) {
+        if (this.checkInInfo.sessionEndTime === '') {
             this.checkInInfo.sessionEndTime = new Date();
         } else {
             this.checkInInfo.sessionEndTime = '';
         }
         Meteor.call('saveCheckInInfo', this._id, this.checkInInfo);
     },
-    'click #checkInBtn': function(e){
-        if(this.checkInInfo.proctorCheckInTime === '') {
+    'click #checkInBtn': function (e) {
+        if (this.checkInInfo.proctorCheckInTime === '') {
             this.checkInInfo.proctorCheckInTime = new Date();
         }
-        else{
+        else {
             this.checkInInfo.proctorCheckInTime = '';
         }
         Meteor.call('saveCheckInInfo', this._id, this.checkInInfo);
     },
-    'click #attendees10': function(e){
+    'click #attendees10': function (e) {
         _editAttendanceType = 10;
         _modalDep.changed();
         $("#myModal").modal();
     },
-    'click #attendees50': function(e){
+    'click #attendees50': function (e) {
         _editAttendanceType = 50;
         _modalDep.changed();
         $("#myModal").modal();
     },
-    'click .numberButton': function(e){
+    'click .numberButton': function (e) {
         var num = $(e.target).text().split('');
 
-        if(_editAttendanceType === 10){
+        if (_editAttendanceType === 10) {
             var oldNum = this.checkInInfo.attendees10.toString().padZero(3).split('');
-            oldNum[3-num.length] = oldNum[3-num.length] === num[0] ? '0' : num[0];
+            oldNum[3 - num.length] = oldNum[3 - num.length] === num[0] ? '0' : num[0];
             this.checkInInfo.attendees10 = parseInt(oldNum.join(''));
         }
-        else{
+        else {
             var oldNum = this.checkInInfo.attendees50.toString().padZero(3).split('');
-            oldNum[3-num.length] = oldNum[3-num.length] === num[0] ? '0' : num[0];
+            oldNum[3 - num.length] = oldNum[3 - num.length] === num[0] ? '0' : num[0];
             this.checkInInfo.attendees50 = parseInt(oldNum.join(''));
         }
         Meteor.call('saveCheckInInfo', this._id, this.checkInInfo);
         _modalDep.changed();
     },
-    'blur #notes': function(e){
+    'blur #notes': function (e) {
         this.checkInInfo.notes = $('#notes').val().trim();
         Meteor.call('saveCheckInInfo', this._id, this.checkInInfo);
     }
