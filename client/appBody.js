fdc6f1a086459866be7e97c046c0a0880af5f71a
Scott Zischerk <szischerk@ibs.com>
2015-11-11 19:21:58 -0500

    Cleaned up UI of home page

--- client/appBody.js
+++ client/appBody.js
@@ -1,6 +1,7 @@
 Template.navigation.helpers({
     isAdminRole: function(){
 
+        if(!Meteor.user()) return false;
         return Meteor.user().role === 'admin' || false;
     }
 });
--- features/home/client/home.js
+++ features/home/client/home.js
@@ -4,5 +4,13 @@ Template.home.helpers({
     },
     isCheckedIn:function(session){
         return !(session.checkInInfo.proctorCheckInTime == null || session.checkInInfo.proctorCheckInTime == '');
+    },
+    getProctor:function(proctorIds){
+        var proctorString = '';
+        for(var i = 0; i < proctorIds.length; i++){
+            var user = Meteor.users.find(proctorIds[i]).fetch()[0];
+            proctorString += user.profile.lastName;
+        }
+        return proctorString;
     }
 });
