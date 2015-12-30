a29fc8f711aeba025da58485f555fa945fc81dcb
Scott Zischerk <szischerk@ibs.com>
2015-12-30 16:59:14 -0500

    Added responsibilities page, changed some of the navigtion

--- client/lib/router.js
+++ client/lib/router.js
@@ -4,7 +4,8 @@ Router.configure({
 });
 
 Router.map(function () {
-    this.route('home', {path: '/'});
+    this.route('schedule', {path: '/'});
+    this.route('home', {path: '/dashboard'});
     this.route('groups');
     this.route('group', {path: '/group/:groupName'});
     this.route('listUsers', {path: '/users'});
@@ -13,11 +14,12 @@ Router.map(function () {
     this.route("staticAdmin", {path: '/admin/static'});
     this.route("staticAdminEdit", {path: '/admin/static/:_id'});
     this.route("adminHelpers", {path: '/admin/helpers'});
-    this.route('schedule', {path: '/schedule'});
+
     this.route('checkinsession', {path: '/checkin/:sessionId'});
     this.route('messages', {path: '/messages'});
     this.route('sendmessages', {path: '/messages/sendmessages'});
     this.route('account', {path: '/account'});
     this.route('exportsessiondata', {path: '/exportsessiondata'});
     this.route('allschedules', {path: '/allschedules'});
+    this.route('responsibilities', {path: '/responsibilities'});
 });
